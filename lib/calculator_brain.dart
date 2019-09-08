d6a7385c89017b7c2a5f449e50e6dbff298a2bde
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-08 16:50:48 +0530

    Add calculator brain with logic to calcuate BMI

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  final int height;
+  final int weight;
+  double _bmi;
+
+  CalculatorBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    _bmi = this.weight / pow(this.height / 100, 2);
+    return _bmi.toStringAsFixed(2);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to exercice more.';
+    } else if (_bmi > 18.5) {
+      return 'You have normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more.';
+    }
+  }
+}
